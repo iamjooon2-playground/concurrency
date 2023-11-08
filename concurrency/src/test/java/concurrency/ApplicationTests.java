d3620f9ade2eb4bdd46425838ec09dff446e15ad
iamjooon2 <iamjooon2@gmail.com>
2023-11-08 17:57:40 +0900

    initial commit

---
+++ concurrency/src/test/java/concurrency/ApplicationTests.java
@@ -0,0 +1,13 @@
+package concurrency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
